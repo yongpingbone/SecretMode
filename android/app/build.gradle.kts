2d0e7d4317569342ff3d8babcfff15e2e7c45eac
yongpingbone <yihanhsu123@gmail.com>
2026-08-26 18:31:54 +0800

    test(android): configure custom JNI probe instrumentation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,6 +12,7 @@ android {
         targetSdk = 36
         versionCode = 1
         versionName = "0.0.1-m0"
+        testInstrumentationRunner = "com.yongpingbone.secretmode.crypto.CryptoProbeInstrumentation"
     }
 
     buildTypes {
